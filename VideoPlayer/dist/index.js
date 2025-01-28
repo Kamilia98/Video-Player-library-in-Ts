7031512aa213d2aea820f7b10ce5a078911acfd5
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-01-28 04:14:07 +0200

    Initial commit

---
+++ VideoPlayer/dist/index.js
@@ -0,0 +1,11 @@
+import VideoPlayer from '../node_modules/video-player/index.js';
+const videoPlayer = new VideoPlayer({
+    containerId: 'videoPlayerContainer',
+    videos: [
+        'https://www.w3schools.com/html/mov_bbb.mp4',
+        'https://media.w3.org/2010/05/sintel/trailer.mp4',
+    ],
+    theme: {
+        color: 'red',
+    },
+});
